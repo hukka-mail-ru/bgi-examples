9d7f86d18a36f0e04a3c690baa74b30c72093512
Sergei <hukka@mail.ru>
2020-09-17 18:01:23 +0300

    Update aim.cpp

--- aim.cpp
+++ aim.cpp
@@ -1,5 +1,35 @@
 #include <graphics.h>
 
+void drawSomething(int x, int y)
+{
+    setcolor(5);
+    x = mousex();
+    y = mousey();
+
+    moveto(x - 50, y - 50);
+    lineto(x + 50, y - 50);
+    lineto(x + 50, y + 50);
+    lineto(x - 50, y + 50);
+    lineto(x - 50, y - 50);
+}
+
+void drawCross(int x, int y)
+{
+    cleardevice();
+    setcolor(3);
+    x = mousex();
+    y = mousey();
+
+    for (int i = 0; i < 10; i++)
+    {
+        circle(x, y, i * 3);
+    }
+
+    line(x, y-50, x, y+50);
+    line(x-50, y, x+50, y);
+}
+
+
 WINDOWS_MAIN() //  "int main()", but for Windows 
 {
     initwindow(640, 480);
@@ -12,6 +42,9 @@ WINDOWS_MAIN() //  "int main()", but for Windows
 
     line(200, 50, 200, 350);
     line(50, 200, 350, 200);
+
+    registermousehandler(WM_MOUSEMOVE, drawCross);
+    registermousehandler(WM_LBUTTONDOWN, drawSomething);
 
 
     getch(); // wait until we close the window
