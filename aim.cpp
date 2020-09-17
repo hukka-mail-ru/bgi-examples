8f24fc8ebc2f762b532accf045fb2b4649baa624
Sergei <hukka@mail.ru>
2020-09-17 16:11:30 +0300

    added aim project

---
+++ aim.cpp
@@ -0,0 +1,18 @@
+#include <graphics.h>
+
+WINDOWS_MAIN() //  "int main()", but for Windows 
+{
+    initwindow(640, 480);
+    setcolor(3);
+
+    for (int i = 0; i < 10; i++)
+    {
+        circle(200, 200, i * 10);
+    }
+
+    line(200, 50, 200, 350);
+    line(50, 200, 350, 200);
+
+
+    getch(); // wait until we close the window
+}
