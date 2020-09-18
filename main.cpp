b092bf0bbf37e0f1966172ccd8f43fec8e4d66c4
Sergei <hukka@mail.ru>
2020-09-18 14:20:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,16 +5,16 @@ const int h = 45;
 
 void drawDigit(int digit, int x, int y)
 {
-    if (digit == 0) readimagefile("pics\\0.bmp", x, y, x + w, y + h);
-    if (digit == 1) readimagefile("pics\\1.bmp", x, y, x + w, y + h);
-    if (digit == 2) readimagefile("pics\\2.bmp", x, y, x + w, y + h);
-    if (digit == 3) readimagefile("pics\\3.bmp", x, y, x + w, y + h);
-    if (digit == 4) readimagefile("pics\\4.bmp", x, y, x + w, y + h);
-    if (digit == 5) readimagefile("pics\\5.bmp", x, y, x + w, y + h);
-    if (digit == 6) readimagefile("pics\\6.bmp", x, y, x + w, y + h);
-    if (digit == 7) readimagefile("pics\\7.bmp", x, y, x + w, y + h);
-    if (digit == 8) readimagefile("pics\\8.bmp", x, y, x + w, y + h);
-    if (digit == 9) readimagefile("pics\\9.bmp", x, y, x + w, y + h);
+    if (digit == 0) readimagefile("pics/0.bmp", x, y, x + w, y + h);
+    if (digit == 1) readimagefile("pics/1.bmp", x, y, x + w, y + h);
+    if (digit == 2) readimagefile("pics/2.bmp", x, y, x + w, y + h);
+    if (digit == 3) readimagefile("pics/3.bmp", x, y, x + w, y + h);
+    if (digit == 4) readimagefile("pics/4.bmp", x, y, x + w, y + h);
+    if (digit == 5) readimagefile("pics/5.bmp", x, y, x + w, y + h);
+    if (digit == 6) readimagefile("pics/6.bmp", x, y, x + w, y + h);
+    if (digit == 7) readimagefile("pics/7.bmp", x, y, x + w, y + h);
+    if (digit == 8) readimagefile("pics/8.bmp", x, y, x + w, y + h);
+    if (digit == 9) readimagefile("pics/9.bmp", x, y, x + w, y + h);
 }
 
 
@@ -23,7 +23,7 @@ int WIN_MAIN() //  main() for Windows
 {
     initwindow(540, 540);
     
-    readimagefile("pics\\watch.bmp", 0, 0, 540, 540);
+    readimagefile("pics/watch.bmp", 0, 0, 540, 540);
 
     for (int i = 0; i < 10; i++)
     {
