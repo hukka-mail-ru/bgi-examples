63d31d8bf9cb4db699c3eda057843b73d6013cfa
Sergei <hukka@mail.ru>
2020-09-18 12:05:26 +0300

    Update aim.cpp

--- aim.cpp
+++ aim.cpp
@@ -1,19 +1,44 @@
 #include <graphics.h>
 
+void drawAim(int x, int y)
+{
+    setcolor(CYAN);
+
+    for (int i = 0; i < 10; i++)
+    {
+        circle(x, y, i * 3);
+    }
+}
+
+void drawHit(int x, int y)
+{
+    setcolor(6);
+    x = mousex();
+    y = mousey();
+
+    setfillstyle(LINE_FILL, LIGHTMAGENTA);
+    fillellipse(x, y, 50, 50);
+
+    drawAim(x, y);
+}
+
+void mouseMove(int x, int y)
+{
+    x = mousex();
+    y = mousey();
+
+    cleardevice();
+    drawAim(x, y);
+}
+
 
 int WIN_MAIN() //  main() for Windows 
 {
     initwindow(640, 480);
     setcolor(3);
 
-    for (int i = 0; i < 10; i++)
-    {
-        circle(200, 200, i * 10);
-    }
-
-    line(200, 50, 200, 350);
-    line(50, 200, 350, 200);
-
+    registermousehandler(WM_MOUSEMOVE, mouseMove);
+    registermousehandler(WM_LBUTTONDOWN, drawHit);
 
     getch(); // wait until we close the window
 }
