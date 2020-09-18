ca4218aa0e8582ae3d327854e90b572a26ee1f05
Sergei <hukka@mail.ru>
2020-09-18 16:14:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,10 +8,25 @@ int WIN_MAIN() //  main() for Windows
     initwindow(xsize, ysize);
     bar(0, 0, xsize, ysize);
 
-    // coordinate grid:
+
+    // grid: 
     setcolor(LIGHTGRAY);
-    line(20, ysize/2, xsize-20, ysize / 2);
-    line(xsize/2, 20, xsize / 2, ysize-20);
+    setlinestyle(DOTTED_LINE, 0, 1);
+    for (int i = 0; i < xsize; i+=20) // vertical lines   
+    {
+        line(i, 0, i, ysize);
+    }
+    for (int i = 0; i < ysize; i += 20) // horizontal lines
+    {
+        line(0, i, xsize, i);
+    }
+
+
+    // coordinate axis: 
+    setcolor(DARKGRAY);
+    setlinestyle(SOLID_LINE, 0, 1);
+    line(0, ysize / 2, xsize, ysize / 2);
+    line(xsize / 2, 0, xsize / 2, ysize);
 
 
     // parabolas:
@@ -21,7 +36,7 @@ int WIN_MAIN() //  main() for Windows
         setcolor( i % 11 + 5);
         for (double x = -100; x < 100; x++)
         {
-            double y = x * x / (50 - i*10) - (i*10 - 50);
+            double y = x * x / (50.0 - i*10.0) - (i*10.0 - 50.0);
 
             double realx = xsize / 2 + x;
             double realy = ysize / 2 - y;
