186708415039b00a8fd12686bea5a40a4f01f1f6
Sergei <hukka@mail.ru>
2020-09-19 09:55:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,14 +6,37 @@ int WIN_MAIN() //  main() for Windows
     initwindow(640, 480);
     setcolor(GREEN);
 
-    for (int i = 0; i < 10; i++)
-    {
-        circle(200, 200, i * 10);
-        delay(250);
-    }
+    // field:
+    int left = 100;
+    int right = 400;
+    int top = 100;
+    int bottom = 400;
 
-    line(200, 50, 200, 350);
-    line(50, 200, 350, 200);
+    // ball:
+    int x = 200;
+    int y = 200;
+    int radius = 30;
+
+    // movement:
+    int dx = 1;
+    int dy = 2;
+
+
+    for (;; x += dx, y += dy) // endless loop
+    {
+        cleardevice();
+
+        rectangle(left, top, right, bottom);
+        circle(x, y, radius);
+
+        if (x > right - radius) dx = -dx;
+        if (x < left + radius) dx = -dx;
+
+        if (y < top + radius) dy = -dy;
+        if (y > bottom - radius) dy = -dy;
+
+        delay(10);
+    }
 
 
     getch(); // wait until we close the window
