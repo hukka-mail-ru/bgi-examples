d71b3bc7910e395dca0bf67c7bf90dc3183cc077
Sergei <hukka@mail.ru>
2020-09-19 17:51:56 +0300

    add eyes

--- main.cpp
+++ main.cpp
@@ -4,18 +4,23 @@
 void onMouseMove(int x, int y)
 {
     cleardevice();
-    setbkcolor(WHITE);
-
     readimagefile("simpson.bmp", x, y, x + 250, y + 250);
 }
 
-
+void onMouseClick(int x, int y)
+{
+    cleardevice();
+    readimagefile("simpson_eyes.bmp", x, y, x + 250, y + 250);
+}
 
 int WIN_MAIN() //  main() for Windows 
 {
     initwindow(500, 500);
+    setbkcolor(WHITE);
 
     registermousehandler(WM_MOUSEMOVE, onMouseMove);
+    registermousehandler(WM_LBUTTONDOWN, onMouseClick);
+    registermousehandler(WM_LBUTTONUP, onMouseMove);
 
     getch(); // wait until we close the window
 }
