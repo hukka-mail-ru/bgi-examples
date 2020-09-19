ccd16326652a9d7848d3a80fce6ed6c4fd2ca808
Sergei <hukka@mail.ru>
2020-09-19 09:39:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <graphics.h>
-<<<<<<< Updated upstream
-=======
 
-int eyes = 0;
+
 int smile = 0;
 
 void drawFace(int x, int y)
@@ -22,10 +20,6 @@ void drawFace(int x, int y)
 
     if (smile)
     {
-        setfillstyle(SOLID_FILL, WHITE);
-        fillellipse(eyes - 20, 150, 10, 10);
-        fillellipse(eyes + 20, 150, 10, 10);
-
         setlinestyle(SOLID_LINE, 0, 10);
         arc(200, 200, 180, 0, smile);
     }
@@ -39,32 +33,19 @@ void onMouseMove(int x, int y)
 
 void onMousePress(int x, int y)
 {
-    eyes = x;
     smile = y;
 }
->>>>>>> Stashed changes
 
 
 int WIN_MAIN() //  main() for Windows 
 {
-<<<<<<< Updated upstream
-    initwindow(640, 480);
-    setcolor(3);
 
-    for (int i = 0; i < 10; i++)
-    {
-        circle(200, 200, i * 10);
-        delay(250);
-    }
-
-    line(200, 50, 200, 350);
-    line(50, 200, 350, 200);
-=======
     initwindow(400, 400 );
 
     drawFace(40, 200);
->>>>>>> Stashed changes
 
+    registermousehandler(WM_MOUSEMOVE, onMouseMove);
+    registermousehandler(WM_LBUTTONDOWN, onMousePress);
 
     getch(); // wait until we close the window
 }
