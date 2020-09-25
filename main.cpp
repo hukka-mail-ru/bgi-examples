dac4b2deb0a5d5ab6634f31745736315e6c27c06
Sergei <hukka@mail.ru>
2020-09-25 09:05:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,31 +9,33 @@ int WIN_MAIN() //  main() for Windows
     bar(0, 0, xsize, ysize);
 
 
-    // grid: 
+    // Рисуем сетку: 
     setcolor(LIGHTGRAY);
-    setlinestyle(DOTTED_LINE, 0, 1);
-    for (int i = 0; i < xsize; i+=20) // vertical lines   
+    setlinestyle(DOTTED_LINE, 0, 1); // Стиль линии: пунктирная, толщина 1
+
+    for (int i = 0; i < xsize; i+=20) // вертикальные линии  
     {
         line(i, 0, i, ysize);
     }
-    for (int i = 0; i < ysize; i += 20) // horizontal lines
+
+    for (int i = 0; i < ysize; i += 20) // горизонтальные лини
     {
         line(0, i, xsize, i);
     }
 
 
-    // coordinate axis: 
+    // Рисуем оси координат: 
     setcolor(DARKGRAY);
-    setlinestyle(SOLID_LINE, 0, 1);
+    setlinestyle(SOLID_LINE, 0, 1); // Стиль линии: сплошная, толщина 1
     line(0, ysize / 2, xsize, ysize / 2);
     line(xsize / 2, 0, xsize / 2, ysize);
 
 
-    // parabolas:
-    setlinestyle(SOLID_LINE, 0, 3);
+    // Рисуем параболы:
+    setlinestyle(SOLID_LINE, 0, 3); // Стиль линии: сплошная, толщина 3
     for (int i = 0; i < 25; i++)
     {
-        setcolor( i % 11 + 5);
+        setcolor( i % 11 + 5); // Цвет параболы меняется с каждой итерацией
         for (double x = -100; x < 100; x++)
         {
             double y = x * x / (50.0 - i*10.0) - (i*10.0 - 50.0);
@@ -43,17 +45,17 @@ int WIN_MAIN() //  main() for Windows
 
             if (x == -100)
             {
-                moveto(realx, realy);
+                moveto(realx, realy); // Начинаем рисовать параболу из точки (realx, realy)
             }
             else
             {
-                lineto(realx, realy);
+                lineto(realx, realy); // Ведем линию в точку (realx, realy)
             }
 
-            delay(1);
+            delay(1); // Пауза 1 миллисекунда
         }
     }
 
 
-    getch(); // wait until we close the window
+    getch(); 
 }
